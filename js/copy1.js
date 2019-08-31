fd4615e701dd938db04e2b63d9c97448cf5b190f
park bitna <bitnapark123@gmail.com>
2019-08-31 12:58:31 +0900

    20190831 / deep copy와 shallow copy 이론

---
+++ js/copy1.js
@@ -0,0 +1,21 @@
+// 같은 메모리 주소를 가졌음을 확인 =>  shallow copy != deep copy
+
+let book = {title: "인사이드 자바스크립트", price: 18000};
+// 1. book을 카피한후에  price를 바꾸면 book과 copyBook이 같이 바뀐다. 이유가 무엇인가?
+// let copyBook = book;
+// copyBook.price = 20000;
+
+
+// 2. book을 카피하되 deep copy를 수행한 후에 price를 30000으로 바꾸고 출력하라. 두개의 값이 달라야 한다.
+// 2-1 Object.assign() 사용하여 deep copy하기
+// 	Object.assign(target, source1, source2, ..., sourceN)
+//						{} 자체가 새로운 메모리 할당
+// const copyBook = Object.assign({}, book, {a: 1}, {title: 40}); <-- title을 override 함
+const copyBook = Object.assign({}, book, {a: 1});
+// book에 있는 property를 {} 안에 복사
+copyBook.price = 20000;
+
+console.log(book);
+console.log(copyBook);
+
+// 2-2 es6 spread 연산자를 사용하여 deep copy 하기
