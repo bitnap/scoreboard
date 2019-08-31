12d1f6f6b7dcdd012177fe583fab7953e7c4b706
park bitna <bitnapark123@gmail.com>
2019-08-31 14:49:37 +0900

    20190831 /uncontrolled Ref로 DOM 값을 얻기

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 export class AddPlayerForm extends React.Component {
-	state = {
-		value: ''
-	}
-
-	handleValueChange = (e) => {
-		this.setState({value: e.target.value});
-	}
+	textInput = React.createRef();
 
 	handleSubmit = (e) => {
 		// submit의 기본 이벤트 막기 // <-- submit 버튼 클릭시 깜빡거림(refreshing) 예방
@@ -20,9 +14,9 @@ export class AddPlayerForm extends React.Component {
 		console.log(player.validity.valid);
 		console.log(form.checkValidity());
 
-
 		// 3) 부모에게서 받은 콜백함수를 호출
-		this.props.addPlayer(this.state.value);
+		// document.getElementById("player"); === this.textInput.current.value
+		this.props.addPlayer(this.textInput.current.value);
 
 
 	}
@@ -31,7 +25,7 @@ export class AddPlayerForm extends React.Component {
 		return (
 			<form id="form" className="form" onSubmit={this.handleSubmit} noValidate>
 				<input id="player" className="input" type="text" placeholder="enter a player's name"
-				value={this.state.value} onChange={this.handleValueChange} required></input>
+				ref={this.textInput} required></input>
 				<input className="input" type="submit" value="Add Player"></input>
 			</form>
 		)
