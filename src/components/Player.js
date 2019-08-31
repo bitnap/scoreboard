78e22ba4caac5e1c7fe0e549c39710cbfc25648b
park bitna <bitnapark123@gmail.com>
2019-08-31 13:18:00 +0900

    20190831 / PureComponent로 사용자 한명만 컨트롤해도 사용자 모두 렌더링 되는 문제를 해결함

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Counter } from './Counter';
 
-export const Player = (props) => (
-	<div className="player">
+export class Player extends React.PureComponent {
+	render() {
+
+
+		console.log(this.props.name, ' rendered');	// 사용자 한명만 컨트롤해도 사용자 모두 렌더링 되는 문제를 해결예정
+
+		return (
+			<div className="player">
 		<span className="player-name">
-			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> x </button>
-			{props.name}
+			<button className="remove-player" onClick={() => this.props.removePlayer(this.props.id)}> x </button>
+			{this.props.name}
 		</span>
-		<Counter score={props.score}
-						 id={props.id}
-						 changeScore={props.changeScore}
-		/>
-	</div>
-)
+				<Counter score={this.props.score}
+								 id={this.props.id}
+								 changeScore={this.props.changeScore}
+				/>
+			</div>
+		)
+	}
+}
